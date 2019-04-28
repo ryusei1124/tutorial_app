dba243f1d0910d18baff2ab1f497d5cb66ac464c
ryusei <ryusei.23342.1124@ezweb.ne.jp>
2019-04-28 14:19:22 +0000

    第10章リファクタリング終了

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -13,4 +13,11 @@ class AttendancesController < ApplicationController
     end
     redirect_to @user
   end
+  
+  def edit
+    @user = User.find(params[:id])
+    @first_day = first_day(params[:date])
+    @last_day = @first_day.end_of_month
+    @dates = user_attendances_month_date
+  end
 end
--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -16,4 +16,12 @@ module AttendancesHelper
   def working_times_sum(seconds)
     format("%.2f", seconds / 60 / 60.0)
   end
+  
+  def first_day(date)
+    !date.nil? ? Date.parse(date) : Date.current.beginning_of_month
+  end
+
+  def user_attendances_month_date
+    @user.attendances.where('worked_on >= ? and worked_on <= ?', @first_day, @last_day).order('worked_on')
+  end
 end
